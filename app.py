7a842e8156414a6636c2641dca62a7b26b615249
Shivang Kesarwani <shivangkesarwani65686@gmail.com>
2026-06-12 12:29:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
-import requests
-from PIL import Image
-from io import BytesIO
+from huggingface_hub import InferenceClient
+import os
 
 st.set_page_config(page_title="AI Text To Image Generator")
 
@@ -9,48 +8,28 @@ st.title("🎨 AI Text To Image Generator")
 
 prompt = st.text_input("Enter your prompt")
 
-API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-
-API_TOKEN = "apply your token hear"
-
-headers = {
-    "Authorization": f"Bearer {API_TOKEN}"
-}
-
 if st.button("Generate Image"):
 
     if prompt:
 
-        with st.spinner("Generating Image..."):
+        try:
+            client = InferenceClient(
+                api_key=os.environ["HF_TOKEN"]
+            )
 
-            try:
-                st.write("Sending request...")
+            image = client.text_to_image(
+                prompt,
+                model="black-forest-labs/FLUX.1-schnell"
+            )
 
-                response = requests.post(
-                    API_URL,
-                    headers=headers,
-                    json={"inputs": prompt},
-                    timeout=60
-                )
+            st.image(
+                image,
+                caption="Generated Image",
+                use_container_width=True
+            )
 
-                st.write(f"Status Code: {response.status_code}")
-
-                if response.status_code == 200:
-
-                    image = Image.open(BytesIO(response.content))
-
-                    st.image(
-                        image,
-                        caption="Generated Image",
-                        use_container_width=True
-                    )
-
-                else:
-                    st.error("Image generation failed.")
-                    st.write(response.text)
-
-            except Exception as e:
-                st.error(f"Error: {e}")
+        except Exception as e:
+            st.error(f"Error: {e}")
 
     else:
         st.warning("Please enter a prompt.")
